7233c8866c55d6b338b3306561c7821ccc8f8ba3
akroms <Akromiphone7.90@gmail.com>
2022-02-06 14:33:39 +0500

    Portfolio page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,57 @@
-/* Selectors and Variables */
-const toggle = document.querySelector(".nav-toggle");
-const navList = document.querySelector(".nav-list");
-/* Event Listeners */
-toggle.addEventListener("click", () => {
-    if (navList.style.animationName === "openAnimation") {
-        navList.style.animationName = "closeAnimation";
+/* Navbar Fixed */
+const navbar = document.querySelector(".navbar");
+
+/* Home */
+const navHome = document.querySelector(".nav-home");
+const homeContent = document.querySelector(".home");
+
+/* Portfolio */
+const navPortfolio = document.querySelector(".nav-portfolio");
+const portfolioBtn = document.querySelector(".portfolio-btn");
+const portfolioContent = document.querySelector(".portfolio");
+
+/* Footer */
+const footerStyle = document.body.querySelector("footer");
+
+/* Home */
+navHome.addEventListener("click", (e) => {
+    homeContent.style.display = "block";
+    footerStyle.style.bottom = "-220px";
+    portfolioContent.style.display = "none";
+    e.preventDefault();
+});
+
+/* Portfolio */
+const navPortfolioStyle = function (e) {
+    portfolioContent.style.display = "block";
+    footerStyle.style.bottom = "-10px";
+    homeContent.style.display = "none";
+    e.preventDefault();
+};
+
+navPortfolio.addEventListener("click", navPortfolioStyle);
+portfolioBtn.addEventListener("click", navPortfolioStyle);
+
+
+/* Scroll Event */
+document.addEventListener("scroll", () => {
+    const navTop = 140;
+    if (window.scrollY > navTop) {
+        navbar.classList.add("navbar-fixed");
     } else {
-        navList.style.animationName = "openAnimation";
+        navbar.classList.remove("navbar-fixed");
     }
 });
 
+/* Event Listener for toggle */
+toggle.addEventListener("click", () => {
+    if (navList.style.animationName === "openAnimation") {
+        navList.style.animationName = "closeAnimation";
+        document.body.style.overflow = "scroll";
+        toggle.innerHTML = togglerImageOpen;
+    } else {
+        navList.style.animationName = "openAnimation";
+        document.body.style.overflow = "hidden";
+        toggle.innerHTML = togglerImageClose;
+    }
+});
---
+++ js/variables.js
@@ -0,0 +1,18 @@
+/* Selectors and Variables */
+const toggle = document.querySelector(".nav-toggle");
+const navList = document.querySelector(".nav-list");
+const togglerImageClose = 
+`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-square-x" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+<rect x="4" y="4" width="16" height="16" rx="2" />
+<path d="M10 10l4 4m0 -4l-4 4" />
+</svg>`;
+
+const togglerImageOpen = 
+`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+<line x1="4" y1="6" x2="20" y2="6"></line>
+<line x1="4" y1="12" x2="20" y2="12"></line>
+<line x1="4" y1="18" x2="20" y2="18"></line>
+</svg>`;
+
